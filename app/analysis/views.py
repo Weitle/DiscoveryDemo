c473ea00fcd898a491739b7a440c99ec0c7d9898
weitao <wangwt62@chinaunicom.cn>
2019-06-28 19:13:45 +0800

    初步实现业务收入按地域分布

--- app/analysis/views.py
+++ app/analysis/views.py
@@ -4,17 +4,17 @@ from flask import request, jsonify
 
 from . import analysis
 from app.data import income_target
+from app.config import DISTRICTS
 
 @analysis.route('/getIncomeMonthly', methods=['POST'])
 def get_income_monthly():
     year = request.form.get('year').strip()
+    month = int(request.form.get('month').strip())
     # 获取年度目标
     target = income_target[year]
     # 随机生成每月收入
     base_income = target/12
-    now = datetime.now()
-    month = now.month
-    incomes = [int(base_income + (random.random()-0.5) * base_income/5) if i < month else 0 for i in range(12)]
+    incomes = [int(base_income + (random.random()-0.5) * base_income/5) for i in range(month)]
     return jsonify({'target':target, 'incomes':incomes})
 
 @analysis.route('/getIncomeBusinessMonthly', methods=['POST'])
@@ -30,3 +30,20 @@ def get_income_business_monthly():
         income_idc = int(8000 + 800 * (random.random()-0.5))
         incomes.append({'2G':income_2G, '3G':income_3G, '4G':income_4G, 'BB':income_bb, 'IDC':income_idc})
     return jsonify({'incomes':incomes})
+
+@analysis.route('/getIncomeDistrictMonthly', methods=['POST'])
+def get_income_district_monthly():
+    year = request.form.get('year').strip()
+    month = int(request.form.get('month').strip())
+    # 获取本年度各分公司收入指标
+    targets = get_targets_by_year(year)
+    income_districts = []
+    for m in range(month):
+        income_districts.append(tuple((target/12*(1+(random.random()-0.5)/10)) for target in targets))
+    return jsonify({'districts':DISTRICTS, 'incomes':income_districts, 'targets':targets})
+
+
+def get_targets_by_year(year):
+    #total = income_target[year]
+    #return {"和平": 38666, "河东":36367, "南开":39654, "河北":31132, "红桥":28932, "河西":46336, "东丽":29535, "津南":23787, "西青":50838, "北辰":29962, "塘沽":72048, "汉沽":11393, "大港":1618, "蓟州":25575, "宝坻":23489, "武清":32227, "静海":29624, "宁河":15572}
+    return (38666, 36367, 46336, 39654, 28932, 31132, 29535, 23787, 50838,29962,72048,11393, 1618, 29624,15572, 32227, 23489,25575)
